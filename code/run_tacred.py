aeb7c389f259c024047c3e45e223153577f0416d
12promise <704157872@qq.com>
2021-11-29 16:01:55 +0800

    第二次修改

--- code/run_tacred.py
+++ code/run_tacred.py
@@ -333,6 +333,7 @@ def main(args):
     id2label = {i: label for i, label in enumerate(label_list)}
     num_labels = len(label_list)
     tokenizer = BertTokenizer.from_pretrained(args.model, do_lower_case=args.do_lower_case)
+    print("sdjhasj")
 
     special_tokens = {}
     if args.do_eval:
